281d8798cf5a04426761ab9f3fd0ea0ae0c407a0
AlejoDevs22 <alejandro.santiago@cajacopieps.com>
2024-11-14 16:06:15 -0500

    html

--- aritmetica.js
+++ aritmetica.js
@@ -1,12 +1,21 @@
-function sumar(a,b){
-    return a + b;
-}
+// function sumar(a,b){
+//     return a + b;
+// }
 
-function restar(a,b){
-    return a - b;
-}
+// function restar(a,b){
+//     return a - b;
+// }
 
-export const aritmetica = {
-        sumar,
-        restar
-    }
+// export const aritmetica = {
+//         sumar,
+//         restar
+//     }
+
+    console.log(document.getElementsByName('barc').value);
+
+    document.addEventListener("change", e => {
+        console.log(e.target);
+        console.log(e.target.value);
+        
+    })
+    
